5e38fb82a0c1b6eead03a4291dc22892fbbfa8ca
harrishjoshi <dev.harishjoshi@gmail.com>
2026-05-24 21:41:21 +0545

    refact(nvim): replace oil with minimal neo-tree setup using smart <C-e> toggle

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,50 +1,22 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		"nvim-tree/nvim-web-devicons",
-	},
-	lazy = false,
-	config = function()
-		require("neo-tree").setup({
-			window = {
-				-- sidebar width in columns
-				width = 30,
-			},
-			filesystem = {
-				filtered_items = {
-					-- show dotfiles dimmed instead of hiding them
-					visible = true,
-					hide_dotfiles = false,
-					hide_gitignored = false,
-				},
-				-- don't prompt to change cwd when revealing files outside cwd
-				bind_to_cwd = false,
-				-- don't use file watcher to avoid cwd change prompts
-				use_libuv_file_watcher = false,
-				follow_current_file = {
-					-- highlight the active file in tree when switching buffers
-					enabled = true,
-				},
-			},
-		})
-
-		-- toggle file tree and reveal current file
-		vim.keymap.set("n", "<C-e>", ":Neotree toggle left reveal<CR>", { desc = "Toggle file explorer" })
-
-		-- open file tree on startup
-		vim.api.nvim_create_autocmd("VimEnter", {
-			callback = function()
-				if vim.fn.argc() == 0 then
-					-- no file opened: show tree, keep focus in tree
-					vim.cmd("Neotree show left")
-				else
-					-- file opened: show tree and reveal the file, keep focus on file
-					vim.cmd("Neotree show left reveal")
-				end
-			end,
-		})
-	end,
-}
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    lazy = false,
+    config = function()
+      vim.keymap.set("n", "<C-e>", function()
+        local path = vim.fn.expand("%:p")
+        if path == "" or vim.fn.isdirectory(path) == 1 then
+          -- no file or directory buffer: use cwd
+          vim.cmd("Neotree toggle left dir=" .. vim.fn.getcwd())
+        else
+          -- single file: root tree at file's parent folder
+          vim.cmd("Neotree toggle left dir=" .. vim.fn.expand("%:p:h"))
+        end
+      end, { desc = "Toggle file explorer" })
+    end
+  }
